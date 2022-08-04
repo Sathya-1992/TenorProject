d1d14865b24cbc42ec4a9c9467c7b8d9c331480f
sathya-1992 <nsathya.gce@gmail.com>
2022-08-04 14:03:20 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,97 @@
+
+let apiDomain = "https://tenor.googleapis.com";
+let apiKey = "AIzaSyBKV66hMoCO9nD0lvEZOGDseK4Cw_pmGG4";
+var trendingEle;
+var root = document.documentElement;
+var rootCS = getComputedStyle(root);
+var parentWidth;
+var leftValue;
+var tags;
+var nextClickCount = 1;
+var previousClickCount;
+var nextButton = document.querySelector(".nextButton");
+var prevButton = document.querySelector(".prevButton");
+
+window.onload = async function(){
+    let trendingGifs = await handleSearchTrendingGifs();
+    renderTrendyGifs(trendingGifs);
+};
+
+function handleSearchTrendingGifs(){
+    let url = apiDomain+"/v2/categories?key="+apiKey+"&type=trending";
+    return fetch(url).then(function(res){
+        return res.json();
+    });
+}
+
+function renderTrendyGifs(data){
+    trendingEle = document.getElementById("parentTenor");
+    tags = data.tags || [];
+    tags.forEach((tagInfo)=>{
+        appendTagInfo(trendingEle,tagInfo.searchterm,tagInfo.image);
+    })
+}
+
+function appendTagInfo(parentEle,imageName,imageUrl){
+    let divEle = document.createElement("div");
+    divEle.classList.add("imageAlign");
+    let imageEle = document.createElement("img");
+    imageEle.setAttribute("src",imageUrl);
+    imageEle.classList.add("trendingGif");
+    let nameEle = document.createElement("div");
+    nameEle.classList.add("imgNameAlign");
+    let name = document.createTextNode(imageName);
+    nameEle.appendChild(name);
+    divEle.appendChild(imageEle);
+    divEle.appendChild(nameEle);
+    parentEle.appendChild(divEle);
+}
+
+function getLeftValue(){
+    parentWidth = trendingEle.offsetWidth;
+    leftValue = parseInt(rootCS.getPropertyValue("--trendingLeft"));
+}
+
+function moveNextTrendingGifs(){
+    getLeftValue();
+    let newValue = leftValue - parentWidth;
+    root.style.setProperty("--trendingLeft",newValue);
+    nextClickCount++;
+    updateButtonDisplay();
+}
+
+function movePreviousTrendingGifs(){
+    getLeftValue();
+    let newValue = leftValue + parentWidth;
+    root.style.setProperty("--trendingLeft",newValue);
+    nextClickCount--;
+    updateButtonDisplay();
+}
+
+function updateButtonDisplay(){
+    if(nextClickCount>1){
+        prevButton.style.display="block";
+    }
+    else if(nextClickCount==1){
+        prevButton.style.display="none";
+    }
+    if(nextClickCount===(tags.length/5)){
+        nextButton.style.display="none";
+    }
+    else if(nextClickCount<(tags.length/5)){
+        nextButton.style.display="block";
+    }
+}
+// const containerElement = document.getElementById('container');
+//     const navElement = document.getElementsByTagName("nav")[0];
+
+//     containerElement.addEventListener("scroll", function(event){
+    
+//         let containerScrollTop = event.target.scrollTop;
+//         let navElementHeight = navElement.offsetHeight;
+        
+//         if(containerScrollTop > navElementHeight){
+//         } else{
+
+//         }
+//     });
---
+++ script.ts
@@ -0,0 +1,9 @@
+
+// if (typeof document !== 'undefined') {
+//     document.querySelector(".container").addEventListener("scroll",headerPosition);
+//     function headerPosition(){
+//         console.log("kjhgfv")
+//     }
+// } else {
+//    console.log('You are on the server')
+// }
